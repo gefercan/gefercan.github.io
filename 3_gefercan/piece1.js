e6a85053aa4b6145ecf1fce7b6812298987b3f9c
gefercan <gefercan@hotmail.com>
2023-10-23 13:47:54 +0400

    Update piece1.js

--- 3_gefercan/piece1.js
+++ 3_gefercan/piece1.js
@@ -28,26 +28,26 @@ window.onload= function(){
 		//textos object with the phrases to be picked
 
 			var textos= {
-					text1:"after the great first click, the buttons are reassigned events but the cache was still there",
-			      	text2:"this might be a little surrealist but mainly there are two figures passing around arguments, dialectically",
-			      	text3:"event listeners are removed and changed by new ones.the same with the second and third click. who will witness?",
-			      	text4:"The piano sounded slowly in the neighbours house but it was only one string",
-			      	text5:"The flowers, always the flowers, surround the statement",
-					text6:"Nevertheless, the dust in the video card does not allow you to see clearly",
-					text7:"and the fragile petals are numbered and indexed",
-					text8:"and the grains of sand area a variable that it is not here, not there, oblique",
-					text9: "but believing the night was here, they went back and throw a stone into the sea",
-					text10:"the birds look at you, thinking: WTF?, but mainly you ignore them, as well",
-					text11:"but you cannot help but scare them, and count them on the sky and forget about it later",
-					text12:"look around the flowering tree that is there, just there",
-					text13:"is this waiting the time,<br>waiting the time, monologous, monotonous",
+					text1:"after the great first click, the buttons are reassigned events but the cache was still there >>>",
+			      	text2:"this might be a little surrealist but mainly there are two figures passing around arguments, dialectically >>> ",
+			      	text3:"event listeners are removed and changed by new ones.the same with the second and third click. who will witness? >>>",
+			      	text4:"The piano sounded slowly in the neighbours house but it was only one string >>>",
+			      	text5:"The flowers, always the flowers, surround the statement >>>",
+					text6:"Nevertheless, the dust in the video card does not allow you to see clearly >>>",
+					text7:"and the fragile petals are numbered and indexed >>>",
+					text8:"and the grains of sand area a variable that it is not here, not there, oblique >>>",
+					text9: "but believing the night was here, they went back and throw a stone into the sea >>>",
+					text10:"the birds look at you, thinking: WTF?, but mainly you ignore them, as well >>>",
+					text11:"but you cannot help but scare them, and count them on the sky and forget about it later >>>",
+					text12:"look around the flowering tree that is there, just there >>>",
+					text13:"is this waiting the time,<br>waiting the time, monologous, monotonous >>>",
 					text14:"waiting the time,<br>waiting the time<br> but somehow<br> living (*)",
 					text15:"as soon as the machine breaths and huff and puff, there it is",
 					text16:"waiting for the sound, the crunch, the elliptical response",
 					text17:"waiting<br>|||||,<br>:::::<br> *X*X*X*X<br> living (*)",
 					text18:"the absurdity of it all, completely, otherwise, completely logical",
-					text19:"waiting<br>|||||,<br>:::::<br> *X*X*X*X<br> living (*)",
-					text20:"waiting<br>|||||,<br>:::::<br> *X*X*X*X<br> living (*)"
+					text19:"but if you found one innocent, might you forgive them all?",
+					text20:"there is not such a thing as the reason you were looking for"
 					}
 
 		//text box where texts are placed
@@ -98,27 +98,40 @@ window.onload= function(){
 			//declaration timeline repetition
 			var tl = gsap.timeline({repeat: 4, repeatDelay: 2});
 			//nube1
-			tl.from("#base3", {x:-500, y:190,opacity: "1",duration: 0.5, delay:0})
+			tl.from("#base3", {x:-500, y:190,opacity: "0.3",duration: 0.5, delay:0})
+			//seresalados1
+			tl.from("#being1", {x:3259,y:490,opacity: "0.3",duration: 9, delay: 0})
+			//letters to the top right
+			tl.to("#a", {x:10,y:450,opacity: "0.2",duration: 3, delay:0});
+			tl.to("#a", {x:20,y:300,opacity: "0.2",duration: 3, delay:0});
 			//nube2
-			tl.from("#base", {x:-500, y:100,opacity: "1",duration: 1, delay:0})
+			tl.from("#base", {x:-500, y:100,opacity: "0.5",duration: 1, delay:0})
 			//nube1
-			tl.to("#base3", {x:2459, y:190,opacity: "1",duration: 9, delay:0.1})
+			tl.to("#base3", {x:2459, y:190,opacity: "0.3",duration: 9, delay:0.1})
 			//nube2
-			tl.to("#base", {x:2459, y:390,opacity: "1",duration: 9, delay:0.1})
-			
+			tl.to("#base", {x:2459, y:390,opacity: "0.5",duration: 9, delay:0.1})
+			//seresalados2
+			tl.from("#being3", {x:-1259, y:990,opacity: "0.3",duration: 1, delay:0.1})
+			tl.to("#being3", {x:3259, y:890, opacity: "0.3",duration: 9, delay: 0.1})
+		
 			console.log("anima0");
 		}
 
 		function anima1(){
 			//declaration timeline
-			var tl = gsap.timeline({repeat: 0, repeatDelay: 0});
+			var tl = gsap.timeline({repeat: 2, repeatDelay: 0});
 			
 			//cratera/flor
-			tl.from("#base4", {x:20,y:200,opacity: "0.3",duration: 1, delay: 2})
+			tl.from("#base4", {x:20,y:200,opacity: "0.8",duration: 1, delay: 0.1})
 			tl.to("#base4", {x:300,y:50,opacity: "1",duration: 1, delay: 0.1})
 			tl.to("#base4", {x:280,y:350,opacity: "0.3",duration: 1, delay: 0.1})
 			tl.to("#base4", {x:180,y:50,opacity: "1",duration: 1, delay:0.1});
-
+			tl.to("#base4", {opacity: "1",duration: 0.3, delay:0.1});
+			tl.to("#base4", {opacity: "0.3",duration: 0.3, delay:0.1});
+			tl.to("#base4", {opacity: "1",duration: 0.3, delay:0.1});
+			tl.to("#base4", {opacity: "0.3",duration: 0.3, delay:0.1});
+			tl.to("#base4", {x:20,y:200,opacity: "1",duration: 0.3, delay:0.1});
+			
 			console.log("anima1");
 		}
 
@@ -129,32 +142,18 @@ window.onload= function(){
 
 			//flower
 			tl.to(".flower", {x:180,y:50,opacity: "0.3",duration: 1, delay: 0.1})
-
-			//seresalados1
-			tl.from("#being1", {x:259,y:890,opacity: "0.1",duration: 1, delay: 0.1})
-			tl.to("#being1", {opacity: "0.3",duration: 1, delay: 0.1})
-			
-			//seresalados2
-			tl.from("#being3", {x:-259, y:890,opacity: "0.1",duration: 1, delay:0.1})
-			tl.to("#being3", {opacity: "0.3",duration: 1, delay: 0.1})
 						
-			tl.to("#base4", {x:-259, y:190,opacity: "0.3",duration: 1, delay: 2})
-			tl.to("#base4", {x:1159, y:390,opacity: "1",duration: 1, delay: 0.1})
-			tl.to("#base4", {x:59, y:890,opacity: "0.3",duration: 1, delay: 0.1})
-			tl.to("#base4", {x:59, y:190,opacity: "1",duration: 1, delay:0.1});
-
+			tl.to("#base4", {x:-259, y:200,opacity: "0.3",duration: 1, delay: 0})
+			tl.to("#base4", {x:159, y:390,opacity: "1",duration: 1, delay: 0.1})
+			tl.to("#base4", {x:59, y:300,opacity: "0.3",duration: 1, delay: 0.1})
+			tl.to("#base4", {x:89, y:200,opacity: "1",duration: 1, delay:0.1});
+			
 			console.log("anima2");
 		}
 
 		function anima3(){
 			var tl = gsap.timeline({repeat:3, repeatDelay: 0.1});
-
-			/* //seresalados1
-			tl.to("#being1", {x:-259,y:-1190,opacity: "0.1",duration: 0.5, delay: 0})
-			tl.to("#being1", {x:159,y:290,opacity: "0.3",duration: 0.5, delay: 0.1})
-			tl.to("#being1", {x:859,y:290,opacity: "0.3",duration: 0.5, delay: 0.1})
-			tl.to("#being1", {x:-1159,y:290,opacity: "0.3",duration: 0.5, delay: 0.1}) */
-			
+	
 			//opacidades
 			tl.to("#base4", {opacity: "0.3",duration: 1, delay: 2})
 			tl.to("#base4", {opacity: "1",duration: 1, delay: 0.1})
@@ -170,6 +169,7 @@ window.onload= function(){
 
 		function anima4(){
 			var tl = gsap.timeline();
+
 			//flower
 			tl.from(".flower", {x:180,y:-250,opacity: "0.3",duration: 0.3, delay: 0})
 			tl.to(".flower", {x:-180,y:-650,opacity: "0.5",duration: 1, delay: 0});
@@ -179,11 +179,33 @@ window.onload= function(){
 
 		function anima5(){
 			var tl = gsap.timeline();
+			tl.to("#a", {x:10,y:450,opacity: "0.2",duration: 4, delay:0});
+			tl.to("#a", {x:20,y:400,opacity: "0.2",duration: 1, delay:0});
 
 
 			console.log("anima5");
 		}
 
+		//MENU
+		function menu(){
+			var tl = gsap.timeline();
+			tl.from("#package1", {x:-9000,opacity: "0",duration: 1, delay:0});
+			tl.to("#package1", {x:100, opacity: "0.9",duration:0.1, delay:0});
+			objetos.poema.style.marginLeft = ("0px");
+			clickingHead(objetos.next,hidemenu);
+			removebuttton(objetos.next,menu);
+			console.log("menu");
+		}
+
+		function hidemenu(){
+			var tl = gsap.timeline();
+			tl.from("#package1", {x:100,opacity: "0.9",duration: 0.1, delay:0});
+			tl.to("#package1", {x:-9000,opacity: "0",duration: 1, delay:0});
+			//objetos.poema.style.marginLeft = ("-9000px");
+	
+			clickingHead(objetos.next,menu);
+			removebuttton(objetos.next,hidemenu);
+		}
 
 		//ACTIVATIONS
 		// hide the cover
@@ -199,10 +221,12 @@ window.onload= function(){
 						objetos.letters2.style.display = ("none");
 						objetos.letters3.style.display = ("block");
 						objetos.mujer.style.display="none";
+						
 
 						objetos.letters3.style.opacity = ("0.3");
-						setTimeout(function(){ objetos.letters3.innerHTML=textos.text1; }, 1000);
-						setTimeout(function(){ txts.innerHTML=textos.text2; }, 1300);
+						setTimeout(function(){ objetos.letters3.innerHTML=textos.text1+textos.text18; }, 3000);
+						setTimeout(function(){ objetos.letters3.innerHTML=textos.text9+textos.text17; }, 6000);
+						setTimeout(function(){ txts.innerHTML=textos.text2; }, 1500);
 						
 						//callback functions
 						fallingFlowers();
@@ -210,6 +234,8 @@ window.onload= function(){
 
 						removebuttton(objetos.anfora,hide1);
 						clickingHead (objetos.anfora,hide2);
+
+						clickingHead (objetos.next,menu);
 
 						removebuttton(objetos.letters3,hide1);
 						clickingHead (objetos.letters3,hide2);
@@ -232,15 +258,12 @@ window.onload= function(){
 						objetos.letters2.style.display = ("none");
 						objetos.letters3.style.display = ("block");
 						objetos.letters1.innerHTML="but there is some emotion" + textos.text6;
-						setTimeout(function(){ objetos.letters1.innerHTML=textos.text12; }, 2000);
-						objetos.next.innerHTML="peripheric";
-						setTimeout(function(){ objetos.next.innerHTML=textos.text3; }, 1000);
-						setTimeout(function(){ objetos.next.innerHTML="next"; },2000);
+						setTimeout(function(){ objetos.letters1.innerHTML=textos.text12; }, 3900);
+						setTimeout(function(){ objetos.letters2.innerHTML=textos.text3; }, 1900);
 						objetos.letters3.innerHTML="nowhere is still available the signal or the sound, " + textos.text7;
 						setTimeout(function(){ objetos.letters3.innerHTML=textos.text16 + "" + textos.text15; },2500);
 						
-						objetos.being1.style.display="block";
-						objetos.being3.style.display="block";
+				
 						objetos.anfora.style.display="block";
 						objetos.mujer.style.display="none";
 						
@@ -272,19 +295,20 @@ window.onload= function(){
 						objetos.anfora.style.display="none";
 						objetos.mujer.style.display="block";
 					
+
 						objetos.letters1.innerHTML=textos.text10; 	
 						objetos.letters2.innerHTML=textos.text2;
-						setTimeout(function(){ objetos.letters1.innerHTML=textos.text8; }, 2000);
+						setTimeout(function(){ objetos.letters1.innerHTML=textos.text4; }, 2000);
 
-						objetos.letters3.innerHTML=textos.text5;
-						setTimeout(function(){ objetos.letters3.innerHTML=textos.text9; }, 2000);
+						objetos.letters3.innerHTML=textos.text4;
+						setTimeout(function(){ objetos.letters3.innerHTML=textos.text5; }, 2000);
 
-						txts.innerHTML=textos.text7;
-						setTimeout(function(){ txts.innerHTML=textos.text5; }, 2000);
+						txts.innerHTML=textos.text6;
+						setTimeout(function(){ txts.innerHTML=textos.text7; }, 2000);
 
-						setTimeout(function(){ objetos.next.innerHTML=textos.text13; }, 1000);
-						setTimeout(function(){ objetos.next.innerHTML="next"; },2000);
+					
 
+						objetos.fondo.style.backgroundImage=("linear-gradient(rgba(15,190,200,1), rgba(255,0,0,0)");
 						objetos.fondo.style.backgroundColor=("blue");
 
 						anima3();
@@ -296,20 +320,17 @@ window.onload= function(){
 
 
 		function hide4(){
-						var pajaro1 = document.getElementById("being1");
-						var pajaro2 = document.getElementById("being3");
 
 						txts.innerHTML=textos.text14;
 					
 						objetos.mujer.style.display="block";
-						pajaro1.style.display="none";
-						pajaro2.style.display="none";
 						objetos.letters3.innerHTML= textos.text4;
-						objetos.poema.style.display="block";
-						objetos.poema.style.zIndex="-10";
+					/* 	objetos.poema.style.display="block";
+						objetos.poema.style.zIndex="-10"; */
 
+						objetos.fondo.style.backgroundImage=("linear-gradient(rgba(155,120,20,1), rgba(255,0,0,0)");
 						objetos.fondo.style.backgroundColor=("yellow");
-
+						
 						anima4();
 						fallingFlowers()
 						removebuttton(objetos.mujer,hide4);
@@ -321,9 +342,14 @@ window.onload= function(){
 		function hide5(){
 						
 						
-						objetos.fondo.style.backgroundImage="url('https://2.bp.blogspot.com/-cN9MC34hsPs/U7_RMjq1_II/AAAAAAAABUs/3hN-amdf670/s1600/2erta.jpg')";
+						//objetos.fondo.style.backgroundImage="url('https://2.bp.blogspot.com/-cN9MC34hsPs/U7_RMjq1_II/AAAAAAAABUs/3hN-amdf670/s1600/2erta.jpg')";
+						objetos.fondo.style.backgroundColor=("magenta");
+						objetos.fondo.style.backgroundImage=("linear-gradient(red, rgba(255,0,0,0)");
 						objetos.letters3.innerHTML=textos.text11;
 						objetos.mujer.style.display="block";
+						
+						setTimeout(function(){ objetos.letters2.innerHTML=textos.text13+textos.text1; }, 2500);
+						setTimeout(function(){ objetos.letters2.innerHTML=textos.text120+textos.text12; },5000);
 						anima5();
 						fallingFlowers()
 						resizeSize(anima1,anima2);
@@ -381,6 +407,8 @@ window.onload= function(){
 		
 }
 //check this https://gsap.com/community/forums/topic/25254-increasing-timeline-value-on-mouse-move/
+
+
 
 			cover();
 
