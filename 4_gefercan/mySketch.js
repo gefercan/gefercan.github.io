310615af4ac5e56789dbfed31b0044e96bebcddf
gefercan <gefercan@hotmail.com>
2025-05-26 17:09:33 +0400

    Update mySketch.js

--- 4_gefercan/mySketch.js
+++ 4_gefercan/mySketch.js
@@ -52,7 +52,7 @@ function setup() {
 function draw() {
 	
 	
-	if(frameCount<(fxrand()*100)){
+	if(frameCount<(fxrand()*1500)){
 		//background variation
 		background(palette[fxmap1],palette2[fxmap1]*2,fxcolourmap);
 		//color and transparence of video
