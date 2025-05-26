16098364a7c65baf7e4e60e6476b4c996bc25c65
gefercan <gefercan@hotmail.com>
2025-05-26 17:00:41 +0400

    Update mySketch.js

--- 4_gefercan/mySketch.js
+++ 4_gefercan/mySketch.js
@@ -153,7 +153,7 @@ function text1(rima){
 		noStroke();
 		fill(250);
 		textSize(32);
-		text(rima,width/4,width/1.7,1600,1500);
+		text(rima,width/4,height/2,1000,1500);
 }
 
 function elements(){
